8e1194a251a2897f706960ed8109d8deff365cb8
wem-r <57370095+wem-r@users.noreply.github.com>
2021-06-08 14:50:37 +0200

    Update Group_Generator.py

--- Misc/Group_Generator.py
+++ Misc/Group_Generator.py
@@ -18,10 +18,11 @@ liste_faces=["Name1", "Name2", "Name3", "Name4", "Name5", "Name6",
 if not (args.groupe == None ):
     nombre_groupes = args.groupe
 else:
+    print("You can use -g followed by the number of groupes")
     print('')
     nombre_groupes = input("Combien de groupes à constituer ? ")
     print('')
-    print("You can use -g followed by the number of groupes")
+
 
 # Fonction de tirage au sort
 def tirage():
