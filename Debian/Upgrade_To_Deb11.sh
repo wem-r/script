425978714d9f6e8c0a7b90865e59bce1279d1b8f
wem-r <57370095+wem-r@users.noreply.github.com>
2021-08-15 21:02:09 +0200

    Add files via upload

---
+++ Debian/Upgrade_To_Deb11.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ $EUID -ne 0 ]; then
+  echo "\e[96m You need to be logged as root to use this script: # sudo $0 \e[0m" 1>&2
+  exit 1
+fi
+
+sed -i 's/buster/bullseye/g' /etc/apt/sources.list
+apt update -y ;  apt full-upgrade -y ; apt dist-upgrade -y
+apt autoremove ; apt autoclean
