cf89e1e866327325e895696a925ed9c366e27679
wem-r <57370095+wem-r@users.noreply.github.com>
2021-08-15 21:38:00 +0200

    Update Upgrade_To_Deb11.sh

--- Debian/Upgrade_To_Deb11.sh
+++ Debian/Upgrade_To_Deb11.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ $EUID -ne 0 ]; then
-  echo "\e[96m You need to be logged as root to use this script: # sudo $0 \e[0m" 1>&2
+  echo "\e[96m You must be logged as root to use this script: # sudo $0 \e[0m" 1>&2
   exit 1
 fi
 
