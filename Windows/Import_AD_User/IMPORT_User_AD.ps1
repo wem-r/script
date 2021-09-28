cbc5a5700eacfb39d2f556e8c628bc7ac3fa3c54
wem-r <57370095+wem-r@users.noreply.github.com>
2021-09-28 11:27:16 +0200

    Update IMPORT_User_AD.ps1

--- Windows/Import_AD_User/IMPORT_User_AD.ps1
+++ Windows/Import_AD_User/IMPORT_User_AD.ps1
@@ -1,5 +1,5 @@
 ﻿Import-Module ActiveDirectory
-$Users = Import-Csv -Delimiter ";" -Path "C:\Users\Administrator\Desktop\1K_Users_2.csv"
+$Users = Import-Csv -Delimiter ";" -Path "C:\Users\Administrator\Desktop\1K_Users.csv"
 
 foreach ($User in $Users)
 {
@@ -10,10 +10,11 @@ foreach ($User in $Users)
     $GivenName = $User.GivenName
     $SAM = $User.SamAccountName
     $UPN = $User.UserPrincipalName
-	$EmailAddress = $User.EmailAdress
+    $EmailAddress = $User.EmailAdress
     $Displayname = $User.DisplayName
     $OU = $User.OU
-	$Server = "dc1.kmr.lan"
+    $Server = "dc1.kmr.lan"
+    $Password = "CHANGE_ME" # If you chose a pwd that doesn't meet the Password Policy (if it wasn't changed) the accounts will be disabled. even when '-Enabled $true' is used
 	
 	Try{
     New-ADOrganizationalUnit -Name $OU -Path "DC=$Domain,DC=$Ext"
@@ -24,7 +25,7 @@ foreach ($User in $Users)
         echo "OU $OU déjà existante"
     }
 	Try{
-	New-ADUser -Surname $Surname -Name $Name -GivenName $GivenName -SamAccountName $SAM -UserPrincipalName $UPN -EmailAddress $EmailAddress -DisplayName $Displayname -AccountPassword:(ConvertTo-SecureString -AsPlainText P@$sW0rD -Force) -Enabled $true -Path "OU=$OU,DC=$Domain,DC=$Ext" -ChangePasswordAtLogon $false –PasswordNeverExpires $true -server $Server
+	New-ADUser -Surname $Surname -Name $Name -GivenName $GivenName -SamAccountName $SAM -UserPrincipalName $UPN -EmailAddress $EmailAddress -DisplayName $Displayname -AccountPassword:(ConvertTo-SecureString -AsPlainText $Password -Force) -Enabled $true -Path "OU=$OU,DC=$Domain,DC=$Ext" -ChangePasswordAtLogon $false –PasswordNeverExpires $true -server $Server
 	
         echo "Utilisateur ajouté : $Name"
 	}
