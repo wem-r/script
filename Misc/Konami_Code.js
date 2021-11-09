fdcae917b1b88f6822d60fbb3ef0888b3e53e7ab
wem-r <57370095+wem-r@users.noreply.github.com>
2021-11-09 14:34:59 +0100

    Create Konami_Code.js

---
+++ Misc/Konami_Code.js
@@ -0,0 +1,17 @@
+function On_Konami_Code(cb) {
+    var input = '';
+    var key = '38384040373937396665';
+    document.addEventListener('keydown', function (e) {
+      input += ("" + e.keyCode);
+      if (input === key) {
+        return cb();
+      }
+      if (!key.indexOf(input)) return;
+      input = ("" + e.keyCode);
+    });
+  }
+
+  On_Konami_Code(function P() { var pass=prompt("Password ?");
+  var _cs=["\x66\x75\x6e\x63","\x54\x53","\x53\x52","\x32\x30"]; if (pass == _cs[1]+_cs[2]+_cs[3]+_cs[3]) { location = "https://www.youtube.com/watch?v=dQw4w9WgXcQ" }
+else alert("Try Again !!!");
+})
