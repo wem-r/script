ec3f4a2c4a6110da5ef818078fcb56a035e54f68
wem-r <57370095+wem-r@users.noreply.github.com>
2022-08-26 22:01:56 +0200

    Update LAMPs_GLPI_install.sh

--- Debian/Deb11/LAMPs_GLPI_install.sh
+++ Debian/Deb11/LAMPs_GLPI_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Script full install LAMPs + GLPI on Debian 10
-# Wemy - 24/04/2021
-# https://wemy.ninja/ https://ais.wemy.ninja/ & https://github.com/wem-r/script
+# Wemy - 25/08/2022
+# https://github.com/wem-r/script
 clear 
 
 if [ $EUID -ne 0 ]; then
